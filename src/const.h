1da40019c41f181b5d54f57b07d7fec2d7e0e9a7
Simon Xiao <sixiao@microsoft.com>
2019-07-11 16:15:26 -0700

    version 0.2.0

--- src/const.h
+++ src/const.h
@@ -5,7 +5,7 @@
 // ----------------------------------------------------------------------------------
 
 #define TOOL_NAME "lagscope"
-#define TOOL_VERSION "0.1.2"
+#define TOOL_VERSION "0.2.0"
 #define AUTHOR_NAME "Shihua (Simon) Xiao, sixiao@microsoft.com"
 
 #define TCP 				SOCK_STREAM
